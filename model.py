1cf1a9e1c69b4de0977bd2384af2447c266acb8b
venus-arman <syousefi81@gmail.com>
2023-10-10 19:55:04 -0400

    Voice records

---
+++ model.py
@@ -0,0 +1,34 @@
+import os
+import speech_recognition as sr
+
+
+class Model_Voice_Text():
+    
+    """
+    This class takes the voices, convert them to text
+    """
+    #open and read the file after the appending:
+    
+    SR_obj = sr.Recognizer()
+   
+    def voice_to_text(self, voicefolder):
+        SR_obj = sr.Recognizer()
+        voicefolder = "C:/Users/syous/OneDrive/Documents/Freelance projects/DataSciencePrpjects/Voice records/"
+        text_list = []
+
+        for subdir, dirs, files in os.walk(voicefolder):
+            for file in files:
+                print(os.path.join(subdir, file))
+                info = sr.AudioFile(os.path.join(subdir, file))
+                print(info)
+       
+                with info as source:
+                    SR_obj.adjust_for_ambient_noise(source)
+                    audio_data = SR_obj.record(source,duration=100)
+                    result = SR_obj.recognize_google(audio_data)
+                    text_list.append(result)
+        return(text_list)
+    
+model = Model_Voice_Text()
+path = "C:/Users/syous/OneDrive/Documents/Freelance projects/DataSciencePrpjects/Voice records/"
+model.voice_to_text(path)
