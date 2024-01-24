58cdd7193391508164bfc4ff9e1b617feec1a36f
Ch Muhammad Awais <37414048+cm-awais@users.noreply.github.com>
2024-01-24 18:00:01 +0100

    Updated Model, added functionality for detecting phone numbers and sin numbers.
    Signed-off-by: Ch Muhammad Awais <37414048+cm-awais@users.noreply.github.com>

--- Mental Health/gradio_app/app.py
+++ Mental Health/gradio_app/app.py
@@ -1,15 +1,13 @@
-# import os
-# import speech_recognition as sr
-# import pickle
-# import nltk
-# from nltk.corpus import wordnet
 import pandas as pd
 import difflib
 import gradio as gr
 from transformers import pipeline
+import librosa
+import re
+
 # import numpy as np
 
-transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base")
+transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-large-v3")
 
 
 # nltk.download('wordnet')
@@ -48,6 +46,29 @@ class Model_Voice_Text():
 
         # Return the number of similar words
         return len(similar_words)
+    
+    def extract_phone_number(self, text):
+        # Define a regular expression pattern to match phone numbers
+        phone_pattern = re.compile(r'\b\d{7,}\b')
+
+        # Search for the phone number in the text
+        match = re.search(phone_pattern, text)
+
+        # Check if a match is found and return the phone number
+        if match:
+            return match.group()
+        else:
+            return "000"
+        
+    def extract_sin(self, text):
+        # Define a regular expression pattern to match phone numbers
+        sin_pattern = re.compile(r'\b\d{4}\b')
+
+        # Search for the phone number in the text
+        matches = re.findall(sin_pattern, text)
+        if matches:
+            return matches 
+        else: return "0000"
 
 
     def matching_text(self, text):
@@ -66,74 +87,73 @@ class Model_Voice_Text():
         if ret == []:
             ret.append("nothing found")
         
+        ph_num = self.extract_phone_number(text=text)
+
+        sin = self.extract_sin(text=text)
+        
+
+        
         # initialize data of lists. 
         data = {'Keywords': [ret], 
                 'Phone Number': ph_num,
                 'SIN': sin,
                 'text': text} 
+        print(data)
         df = pd.DataFrame(data)
-        print(text)
         print(df)
         
         # ret.append(text)
         return df
     
-    def transcribe(self, audio):
+    def transcribe(self, audio_f):
         # sr, y = audio
         # y = y.astype(np.float32)
         # y /= np.max(np.abs(y))
+        # print(type(audio))
+        text = ""
 
-        return transcriber(audio)["text"]
+        # First load the file
+        audio, sr = librosa.load(audio_f)
+
+        # Get number of samples for 20 seconds; replace 20 by any number
+        buffer = 20 * sr
+
+        samples_total = len(audio)
+        samples_wrote = 0
+        counter = 1
+
+        while samples_wrote < samples_total:
+
+            #check if the buffer is not exceeding total samples 
+            if buffer > (samples_total - samples_wrote):
+                buffer = samples_total - samples_wrote
+
+            block = audio[samples_wrote : (samples_wrote + buffer)]
+            # out_filename = "split_" + str(counter) + "_" + audio_f
+
+            # Write 2 second segment
+            # sf.write(out_filename, block, sr)
+
+            # Transcribing the audio to text
+            text += transcriber(block)["text"]
+            counter += 1
+            samples_wrote += buffer
+            # print(counter)
+            # print(text)
+
+        return text
     
     def voice_to_text_s(self, audio):
         # SR_obj = self.SR_obj
         # info = sr.AudioFile(audio)
         tran_text = self.transcribe(audio)
+        # print(tran_text)
         match_results = self.matching_text(tran_text.lower())
         return match_results
-
-        # print(info)
-
-        # with info as source:
-        #     SR_obj.adjust_for_ambient_noise(source)
-        #     audio_data = SR_obj.record(source,duration=100)
-        #     result = SR_obj.recognize_google(audio_data)
-        #     match_results = self.matching_text(result)
-        #     return match_results
-
-   
-    # def voice_to_text(self, voicefolder):
-    #     SR_obj = self.SR_obj
-    #     text_list = []
-    #     res_list = []
-
-    #     for subdir, dirs, files in os.walk(voicefolder):
-    #         for file in files:
-    #             print(os.path.join(subdir, file))
-    #             info = sr.AudioFile(os.path.join(subdir, file))
-    #             print(info)
-       
-    #             with info as source:
-    #                 SR_obj.adjust_for_ambient_noise(source)
-    #                 audio_data = SR_obj.record(source,duration=100)
-    #                 result = SR_obj.recognize_google(audio_data)
-    #                 text_list.append(result)
-    #                 match_results = self.matching_text(result)
-    #                 res_list.append([file, match_results, result])
-
-    #     return(text_list, res_list)
 
 
 model = Model_Voice_Text()
 
-# path = "/home/si-lab/Desktop/Projects/DataSciencePrpjects/Voice_records"
-# text, results = model.voice_to_text(path)
-
-# f = open("demofile2.txt", "a")
-# f.write(text)
-# f.close()
-# df = pd.DataFrame(results)
-# df.to_csv("list.csv", index=False)
 
 demo = gr.Blocks()
 
@@ -154,8 +174,6 @@ with demo:
     )
 
 demo.launch(debug=True)
-
-# pickle.dump(model, open("voice_txt.pkl", "wb"))
 
 
 
